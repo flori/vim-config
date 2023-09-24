dffa3afa99227e0d59bfd993d94e2a71f4986252
Florian Frank <flori@ping.de>
2023-09-24 21:52:31 +0200

    ignore bundle

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 git submodule init
 git submodule update
+git clean -fdx
 ln -sf `pwd`/.vimrc $HOME/.vimrc
 ln -sf `pwd`/.vim $HOME/.vim
 ln -sf `pwd`/vim-plugins $HOME/.vim/bundle
-git clean -fdx
