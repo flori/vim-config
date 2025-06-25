c779c7edc322983d318cc2d45b2118e2c840000b
Florian Frank <flori@ping.de>
2025-06-25 10:13:16 +0200

    Update symbolic links in install script

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 git submodule init
 git submodule update
 git clean -fdx
-ln -sf `pwd`/.vimrc $HOME/.vimrc
-ln -sf `pwd`/.vim $HOME/.vim
-ln -sf `pwd`/vim-plugins $HOME/.vim/bundle
+ln -sf "$(pwd)/.vimrc" "$HOME/.vimrc"
+ln -shf "$(pwd)/.vim" "$HOME/.vim"
+ln -shf "$(pwd)/vim-plugins" "$HOME/.vim/bundle"
