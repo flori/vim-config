d810cb9a28be42217c5d36dfe954b909956921ba
Florian Frank <flori@ping.de>
2025-07-04 04:15:36 +0200

    Use ln -n instead of -h for linux compatibility

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ git submodule init
 git submodule update
 git clean -fdx
 ln -sf "$(pwd)/.vimrc" "$HOME/.vimrc"
-ln -shf "$(pwd)/.vim" "$HOME/.vim"
-ln -shf "$(pwd)/vim-plugins" "$HOME/.vim/bundle"
+ln -snf "$(pwd)/.vim" "$HOME/.vim"
+ln -snf "$(pwd)/vim-plugins" "$HOME/.vim/bundle"
